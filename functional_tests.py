9a584caf5f38fcd59dc28d3ac00a016e980d73b6
ChenHan Leonard Yang <yang313@atrihub.io>
2018-01-22 13:49:00 -0800

    feat: Chapter 5
    This is the completion of chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -5,13 +5,18 @@ import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-    browser = webdriver.Firefox()
+    #browser = webdriver.Firefox()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Someone has heard about a new online feature. She goes to check out their homepage
@@ -36,13 +41,21 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        # She types "Use peacock feathers to make a fly" into a text box
+        inputbox.send_keys('Use peacock feathers to make a fly')
+
+        # When she hits enter, the page updates and page list
+        # "2: Use peacock feathers to make a fly" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock to make a fly"
@@ -57,7 +70,7 @@ class NewVisitorTest(unittest.TestCase):
 
         #Satistied, she goes back to sleep
 
-    browser.quit()
+#    browser.quit()
 
 
 if __name__ == '__main__':
